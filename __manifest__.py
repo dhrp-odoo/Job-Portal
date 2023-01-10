d96e74f267bf79a29473815a23cea21ae022231d
Dhrutik Patel <dhrp@odoo.com>
2023-01-10 14:49:10 +0530

    Created manifest file for Job Portal module.

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Job Portal',
+    'description': 'Job Portal : Create A Better Future For Yourself',
+    'version': '1.0',
+    'category': 'Services',
+    'author': 'Dhrutik Patel (dhrp)',
+    'website': 'https://www.odoo.com/',
+    'summary': 'Job Portal : Create A Better Future For Yourself',
+    'depends': [],
+    'data': [],
+    'demo': [],
+    'installable': True,
+    'application': True,
+}
